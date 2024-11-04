336e660d2018a02afcd9faaaea66eb2b1e01085c
Gupta, Sunny <sunnyvales789@gmail.com>
2024-11-05 00:30:52 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const webPush = require('web-push');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3000;
-require('dotenv').config();
-app.get('/', (req, res) => {
-    // res.send(process.env.SECRET_KEY);
-    res.send('rd');
-    console.log(process.env.SECRET_KEY, 'process.env.SECRET_KEY');
 
+// console.log(process.env.PORT) 
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+var corsOptions = {
+  origin: ["http://localhost:4200","http://127.0.0.1:8081/","www.three.com"],
+  default: "http://localhost:4200"
+};
+app.use(cors(corsOptions));
+// app.options('*', cors()); 
+
+
+app.get('/', (req, res) => {
+    // res.send('rd');
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    // console.log(process.env.SECRET_KEY, 'process.env.SECRET_KEY');
 })
+
+app.post('/subscribe', (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({ title: 'test', body:'body body' });
+
+  console.log(subscription);
+
+  webPush.sendNotification(subscription, payload).catch(error => {
+    console.error(error.stack);
+  });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
 })
---
+++ push-Notifications/server.js
@@ -0,0 +1,29 @@
+const webPush = require('web-push');
+
+// Replace with your own VAPID keys
+const vapidKeys = webPush.generateVAPIDKeys();
+console.log(vapidKeys);
+// webPush.setVapidDetails(
+//   'mailto:example@yourdomain.org',
+//   vapidKeys.publicKey,
+//   vapidKeys.privateKey
+// );
+
+// Example subscription object (should be obtained from the client)
+// const subscription = {
+//   endpoint: 'USER_SUBSCRIPTION_ENDPOINT',
+//   keys: {
+//     auth: 'USER_AUTH_KEY',
+//     p256dh: 'USER_P256DH_KEY',
+//   },
+// };
+
+// Send a push notification
+// const payload = JSON.stringify({
+//   title: 'Hello!',
+//   body: 'This is a push notification from Angular!',
+// });
+
+// webPush.sendNotification(subscription, payload)
+//   .then(response => console.log('Notification sent successfully:', response))
+//   .catch(error => console.error('Error sending notification:', error));
